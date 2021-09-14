a5004d00a699f8a0726b72b3189f8268bd24967b
dartweydr-wq <badproper98@gmail.com>
2021-09-14 11:37:15 +0300

    update. Add cache component

--- class.php
+++ class.php
@@ -7,53 +7,71 @@ use \Bitrix\Main\Loader,
 
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
-class DartTestComponent extends CBitrixComponent {
+class DartTestComponent extends CBitrixComponent
+{
 
     /**
      * Проверка наличия модулей требуемых для работы компонента
      * @return bool
      * @throws Exception
      */
-    private function _checkModules() {
-        if ( !Loader::includeModule("highloadblock") ) {
+    private function _checkModules()
+    {
+        if (!Loader::includeModule("highloadblock")) {
             throw new \Exception('Не загружены модули необходимые для работы компонента');
         }
         return true;
     }
 
-    public function GetHighLoadBlockData() {
+    public function GetHighLoadBlockData()
+    {
         global $USER;
         $arParams = $this->arParams;
 
-        $hlBlockItems = HL\HighloadBlockTable::getById($arParams['HIGHLOAD_TYPE'])->fetch();
-        $entity = HL\HighloadBlockTable::compileEntity($hlBlockItems);
-        $entity_data_class = $entity->getDataClass();
+        $cache_id = md5(serialize($arParams));
+        $cache_dir = '/'.$USER->GetID().'/';
+        $path = '/test22';
+        $obCache = new CPHPCache;
 
-        if($entity_data_class)
+        if($obCache->InitCache($arParams['CACHE_TIME'], $cache_id, $path))
         {
-            $outPropertyBlock = $entity_data_class::getList(array(
-                'select' => ['*'],
-                'filter' => [
-                    '=UF_USER_ID' => $USER->GetID(),
-                    '=UF_USER_ACTIVE' => ($arParams['OUT_USER_ADDRESS'] == 'Y') ? 1 : [1,0]
-                ]
-            ))->fetchAll();
+            $outPropertyBlock = $obCache->GetVars();
+        }
+        elseif ( $obCache->StartDataCache() )
+        {
+            $hlBlockItems = HL\HighloadBlockTable::getById($arParams['HIGHLOAD_TYPE'])->fetch();
+            $entity = HL\HighloadBlockTable::compileEntity($hlBlockItems);
+            $entity_data_class = $entity->getDataClass();
+
+            if ($entity_data_class)
+            {
+                $outPropertyBlock = $entity_data_class::getList(array(
+                    'select' => ['*'],
+                    'filter' => [
+                        '=UF_USER_ID' => $USER->GetID(),
+                        '=UF_USER_ACTIVE' => ($arParams['OUT_USER_ADDRESS'] == 'Y') ? 1 : [1, 0]
+                    ],
+                ))->fetchAll();
+                $obCache->EndDataCache($outPropertyBlock);
+            }
         }
         return $outPropertyBlock;
     }
-    public function CheckModuleParams() {
 
+    public function CheckModuleParams()
+    {
         // проверка модулей
         $this->_checkModules();
 
         // проверка параметров
-        if(!$this->arParams['HIGHLOAD_TYPE'])
+        if (!$this->arParams['HIGHLOAD_TYPE'])
             throw new \Exception('Не выбран HighLoad блок');
 
         return true;
     }
-    
-    public function executeComponent() {
+
+    public function executeComponent()
+    {
         $this->CheckModuleParams();
         $this->arResult['DATA'] = $this->GetHighLoadBlockData();
         $this->includeComponentTemplate();
